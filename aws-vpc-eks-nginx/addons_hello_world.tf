a4e010329e53bdafa3c0d192d2e5fb6d08b011da
ajeannier <alexandre@jeannier.com>
2020-04-02 18:46:29 +0100

    setting for_each loops to use the same object

--- aws-vpc-eks-nginx/addons_hello_world.tf
+++ aws-vpc-eks-nginx/addons_hello_world.tf
@@ -2,27 +2,30 @@
 # two "hello world" apps are returning "Version: 1.0.0" or "Version: 2.0.0"
 # cf https://github.com/GoogleCloudPlatform/kubernetes-engine-samples/tree/master/hello-app
 
-# configuration for ports and labels
+# configuration for ports, labels, and our two applications
 
 locals {
   container_port = 8080
   elb_port       = 80
+  applications = {
+    "hello-world-1" = {
+      "labels" = { application = "hello-world-1" }
+      "image"  = "gcr.io/google-samples/hello-app:1.0"
+      "path"   = "/1"
+    },
+    "hello-world-2" = {
+      "labels" = { application = "hello-world-2" }
+      "image"  = "gcr.io/google-samples/hello-app:2.0"
+      "path"   = "/2"
+    }
+  }
 }
 
-# deployment + service for hello-app:1.0
+# deployment for the two applications
 
 resource "kubernetes_deployment" "deployment_hello_world" {
 
-  for_each = {
-    "hello-world-1" = {
-      "labels" = { application = "hello-world-1" },
-      "image"  = "gcr.io/google-samples/hello-app:1.0"
-    }
-    "hello-world-2" = {
-      "labels" = { application = "hello-world-2" },
-      "image"  = "gcr.io/google-samples/hello-app:2.0"
-    }
-  }
+  for_each = local.applications
 
   metadata {
     name = each.key
@@ -52,18 +55,17 @@ resource "kubernetes_deployment" "deployment_hello_world" {
   ]
 }
 
+# service for the two applications
+
 resource "kubernetes_service" "service_hello_world" {
 
-  for_each = {
-    "hello-world-1" = { application = "hello-world-1" }
-    "hello-world-2" = { application = "hello-world-2" }
-  }
+  for_each = local.applications
 
   metadata {
     name = each.key
   }
   spec {
-    selector = each.value
+    selector = each.value.labels
     port {
       port        = local.elb_port
       target_port = local.container_port
@@ -86,19 +88,19 @@ resource "kubernetes_ingress" "ingress_hello_world" {
       http {
 
         dynamic "path" {
-          for_each = ["1", "2"]
+          for_each = local.applications
           content {
 
-            path = "/${path.value}"
-
+            path = path.value.path
             backend {
-              service_name = "hello-world-${path.value}"
+              service_name = path.key
               service_port = local.container_port
             }
+
           }
         }
+
       }
     }
   }
 }
-
